758eff5b6b4975b6965885e3eebab52bde37c9ab
Gwangyong <key4168@icloud.com>
2025-07-29 16:16:24 +0900

    ✨ SearchBar UI구현 #4
    - BookSearchViewController 내부에 searchBar 구현
    - configureSearchBar()로 자동완성을 끔
    - 자동 수정, 자동 대문자, 맞춤범 검사 끄기

--- AdvanceApp/Presentation/BookSearch/View/BookSearchViewController.swift
+++ AdvanceApp/Presentation/BookSearch/View/BookSearchViewController.swift
@@ -11,18 +11,45 @@ import SnapKit
 import Then
 
 class BookSearchViewController: UIViewController {
-  private let label = UILabel().then {
-    $0.text = "검색 화면"
+  
+  private lazy var searchBar = UISearchBar().then {
+    $0.placeholder = "책 제목"
+    $0.searchBarStyle = .minimal
+    $0.delegate = self
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureUI()
+    configureLayout()
+    configureSearchBar()
+  }
+  
+  // MARK: configureUI
+  private func configureUI() {
     view.backgroundColor = .white
-    view.addSubview(label)
-    label.snp.makeConstraints {
-      $0.center.equalTo(view.safeAreaLayoutGuide)
+    
+    [searchBar].forEach{ view.addSubview($0) }
+  }
+  
+  // MARK: configureLayout
+  private func configureLayout() {
+    searchBar.snp.makeConstraints {
+      $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
     }
   }
   
+  // MARK: - SearchBar 자동완성 끄기
+  private func configureSearchBar() {
+    if let textField = searchBar.value(forKey: "searchField") as? UITextField { // SearchBar에 내부적으로 있는 TextField
+      textField.autocorrectionType = .no       // 자동 수정 끄기
+      textField.autocapitalizationType = .none // 자동 대문자 끄기
+      textField.spellCheckingType = .no        // 맞춤법 검사 끄기
+    }
+  }
+  
+}
 
+extension BookSearchViewController: UISearchBarDelegate {
+  
 }
