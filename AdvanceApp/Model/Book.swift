350a8a9a2c51c27600db572ef2ebde180803b620
Gwangyong <key4168@icloud.com>
2025-07-30 17:32:55 +0900

    ✨ 카카오 책 검색 API 응답 모델 정의 #4
    - Book, Document 구조체 생성
    - 책 제목, 저자, 이미지, 가격, 설명, 번역가 필드 모델링

---
+++ AdvanceApp/Model/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  AdvanceApp
+//
+//  Created by 서광용 on 7/30/25.
+//
+
+struct Book: Codable {
+  let documents: [Document]
+}
+
+struct Document: Codable {
+  let authors: [String]
+  let contents: String
+  let price: Int
+  let thumbnail: String // 이미지 주소 (섬네일)
+  let title: String
+  let translators: [String] // 번역가인데 나중에 시간나면 추가
+}
